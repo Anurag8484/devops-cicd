e49c6a749ec08db0c9dfd415a41c780b1eadcde6
anurag8484 <anuragpoonia.ap@gmail.com>
2025-11-13 11:06:22 +0530

    tweaked fe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,12 +3,16 @@ export default async function Home(){
   const user = await client.user.findFirst();
   return(
     <div>
+      Id:
       {user?.id}
       <br />
+    Name:
       {user?.name}
       <br />
+      Email:
       {user?.email}
       <br />
+      Password:
       {user?.password}
     </div>
   )
